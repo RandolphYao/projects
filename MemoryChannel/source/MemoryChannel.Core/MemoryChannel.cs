e4bdda7e39d2faff263ea5c689423c870ad777cd
RandolphYao <randolph.yao@gmail.com>
2014-02-08 14:04:28 -0800

    Add more tests

--- MemoryChannel/source/MemoryChannel.Core/MemoryChannel.cs
+++ MemoryChannel/source/MemoryChannel.Core/MemoryChannel.cs
@@ -7,34 +7,50 @@
 
     public class MemoryChannel
     {
-        private List<byte> dataRepo;
-
+        // store data send to the channel but did not read by receiver yet
+        private List<byte> excessBuffers;
         private byte[] pendingReceiveBuffer;
         private TaskCompletionSource<int> pendingReceiveTaskSource;
 
         public MemoryChannel()
         {
-            this.dataRepo = new List<byte>(); 
+            this.excessBuffers = new List<byte>(); 
         }
 
         public Task<int> ReceiveAsync(byte[] buffer)
         {
             this.pendingReceiveBuffer = buffer;
             this.pendingReceiveTaskSource = new TaskCompletionSource<int>();
+
+            if (this.excessBuffers.Count > 0)
+            {
+               this.SendDataToReceiver();
+            }
+
             return this.pendingReceiveTaskSource.Task;
         }
 
         public void Send(byte[] buffer)
         {
-            this.dataRepo = this.dataRepo.Concat(buffer).ToList();
+            this.excessBuffers = this.excessBuffers.Concat(buffer).ToList();
 
             if (this.pendingReceiveTaskSource != null)
             {
-                int lengh = this.dataRepo.Count > this.pendingReceiveBuffer.Length ? this.pendingReceiveBuffer.Length : this.dataRepo.Count;               
-                Array.Copy(this.dataRepo.ToArray(), 0, this.pendingReceiveBuffer, 0, lengh);
-                this.dataRepo.RemoveRange(0, lengh);
-                this.pendingReceiveTaskSource.SetResult(lengh);                
+                this.SendDataToReceiver();
             }
+        }
+
+        private void SendDataToReceiver()
+        {
+            int lengh = this.excessBuffers.Count > this.pendingReceiveBuffer.Length
+                            ? this.pendingReceiveBuffer.Length
+                            : this.excessBuffers.Count;
+            Array.Copy(this.excessBuffers.ToArray(), 0, this.pendingReceiveBuffer, 0, lengh);
+            this.excessBuffers.RemoveRange(0, lengh);
+
+            // Note that SetResult will transition the task to a completed state as well run any synchronous continuations. 
+            // This can be undesirable in some situations 
+            this.pendingReceiveTaskSource.SetResult(lengh);
         }
     }
 }
--- MemoryChannel/test/MemoryChannel.Test.Unit/MemoryChannelTest.cs
+++ MemoryChannel/test/MemoryChannel.Test.Unit/MemoryChannelTest.cs
@@ -3,10 +3,11 @@
 // Copyright (c) MyCompany. All rights reserved.
 // </copyright>
 //-----------------------------------------------------------------------
-
 namespace MemoryChannel.Test.Unit
 {
+    using System;
     using System.Threading.Tasks;
+
     using Xunit;
 
     public class MemoryChannelTest
@@ -21,18 +22,261 @@ namespace MemoryChannel.Test.Unit
             MemoryChannel c = new MemoryChannel();
             byte[] receiveBuffer = new byte[3];
             Task<int> receiveTask = c.ReceiveAsync(receiveBuffer);
-
-            Assert.False(receiveTask.IsCompleted);
-            Assert.False(receiveTask.IsFaulted);
+            AssertTaskPending(receiveTask);
 
             var sendBuffer = new byte[] { 1, 2, 3 };
             c.Send(sendBuffer);
 
+            AssertTaskCompleted(3, receiveTask);
+            Assert.Equal(sendBuffer, receiveBuffer);
+        }
+
+        [Fact]
+        public void Pending_ReceiveCompleteAfterSendWithLessDataSize()
+        {
+            MemoryChannel c = new MemoryChannel();
+            byte[] receiveBuffer = new byte[3];
+            Task<int> receiveTask = c.ReceiveAsync(receiveBuffer);
+
+            AssertTaskPending(receiveTask);
+
+            var sendBuffer = new byte[] { 1, 2 };
+            c.Send(sendBuffer);
+
+            AssertTaskCompleted(2, receiveTask);
+            Assert.Equal(new byte[] { 1, 2, 0 }, receiveBuffer);
+        }
+
+        [Fact]
+        public void Pending_receiveCompletes_after_send_with_greater_data_size()
+        {
+            MemoryChannel channel = new MemoryChannel();
+
+            byte[] receiveBuffer = new byte[3];
+            Task<int> receiveTask = AssertTaskPending(channel.ReceiveAsync(receiveBuffer));
+
+            channel.Send(new byte[] { 1, 2, 3, 4 });
+
+            AssertTaskCompleted(3, receiveTask);
+            Assert.Equal(new byte[] { 1, 2, 3 }, receiveBuffer);
+        }
+
+        [Fact]
+        public void Pending_receiveCompletes_afterSendWithGreaterDataSize_followedByAnotherReceiveWithSameDataSize()
+        {
+            MemoryChannel channel = new MemoryChannel();
+
+            byte[] receiveBuffer = new byte[3];
+            Task<int> receiveTask = AssertTaskPending(channel.ReceiveAsync(receiveBuffer));
+
+            channel.Send(new byte[] { 1, 2, 3, 4, 5 });
+
+            AssertTaskCompleted(3, receiveTask);
+            Assert.Equal(new byte[] { 1, 2, 3 }, receiveBuffer);
+
+            byte[] receiveBuffer2 = new byte[2];
+            AssertTaskCompleted(receiveBuffer2.Length, channel.ReceiveAsync(receiveBuffer2));
+            Assert.Equal(new byte[] { 4, 5 }, receiveBuffer2);
+        }
+
+        [Fact]
+        public void Pending_receive_completes_after_send_with_greater_data_size_followed_by_another_receive_with_lower_data_size()
+        {
+            MemoryChannel channel = new MemoryChannel();
+
+            byte[] receiveBuffer = new byte[3];
+            Task<int> receiveTask = AssertTaskPending(channel.ReceiveAsync(receiveBuffer));
+
+            channel.Send(new byte[] { 1, 2, 3, 4, 5 });
+
+            AssertTaskCompleted(3, receiveTask);
+            Assert.Equal(new byte[] { 1, 2, 3 }, receiveBuffer);
+
+            byte[] receiveBuffer2 = new byte[1];
+            AssertTaskCompleted(1, channel.ReceiveAsync(receiveBuffer2));
+            Assert.Equal(new byte[] { 4 }, receiveBuffer2);
+        }
+
+        [Fact]
+        public void Pending_receive_completes_after_send_with_greater_data_size_followed_by_another_receive_with_greater_data_size()
+        {
+            MemoryChannel channel = new MemoryChannel();
+
+            byte[] receiveBuffer = new byte[3];
+            Task<int> receiveTask = AssertTaskPending(channel.ReceiveAsync(receiveBuffer));
+
+            channel.Send(new byte[] { 1, 2, 3, 4, 5 });
+
+            AssertTaskCompleted(3, receiveTask);
+            Assert.Equal(new byte[] { 1, 2, 3 }, receiveBuffer);
+
+            byte[] receiveBuffer2 = new byte[3];
+            AssertTaskCompleted(2, channel.ReceiveAsync(receiveBuffer2));
+            Assert.Equal(new byte[] { 4, 5, 0 }, receiveBuffer2);
+        }
+
+        [Fact]
+        public void Pending_receive_completes_after_send_with_greater_data_size_followed_by_two_receives_to_read_excess()
+        {
+            MemoryChannel channel = new MemoryChannel();
+
+            byte[] receiveBuffer = new byte[3];
+            Task<int> receiveTask = AssertTaskPending(channel.ReceiveAsync(receiveBuffer));
+
+            channel.Send(new byte[] { 1, 2, 3, 4, 5, 6 });
+
+            AssertTaskCompleted(3, receiveTask);
+            Assert.Equal(new byte[] { 1, 2, 3 }, receiveBuffer);
+
+            byte[] receiveBuffer2 = new byte[1];
+            AssertTaskCompleted(1, channel.ReceiveAsync(receiveBuffer2));
+            Assert.Equal(new byte[] { 4 }, receiveBuffer2);
+
+            byte[] receiveBuffer3 = new byte[2];
+            AssertTaskCompleted(2, channel.ReceiveAsync(receiveBuffer3));
+            Assert.Equal(new byte[] { 5, 6 }, receiveBuffer3);
+        }
+
+        [Fact]
+        public void Pending_receive_completes_after_send_with_greater_data_size_followed_by_one_receive_to_read_excess_leaving_final_receive_pending()
+        {
+            MemoryChannel channel = new MemoryChannel();
+
+            byte[] receiveBuffer = new byte[3];
+            Task<int> receiveTask = AssertTaskPending(channel.ReceiveAsync(receiveBuffer));
+
+            channel.Send(new byte[] { 1, 2, 3, 4, 5, 6 });
+
+            AssertTaskCompleted(3, receiveTask);
+            Assert.Equal(new byte[] { 1, 2, 3 }, receiveBuffer);
+
+            byte[] receiveBuffer2 = new byte[3];
+            AssertTaskCompleted(3, channel.ReceiveAsync(receiveBuffer2));
+            Assert.Equal(new byte[] { 4, 5, 6 }, receiveBuffer2);
+
+            byte[] receiveBuffer3 = new byte[1];
+            AssertTaskPending(channel.ReceiveAsync(receiveBuffer3));
+        }
+
+        [Fact]
+        public void Pending_receive_completes_after_send_with_equal_data_size_followed_by_another_send_and_receive()
+        {
+            MemoryChannel channel = new MemoryChannel();
+
+            byte[] receiveBuffer = new byte[3];
+            Task<int> receiveTask = AssertTaskPending(channel.ReceiveAsync(receiveBuffer));
+
+            channel.Send(new byte[] { 1, 2, 3 });
+
+            AssertTaskCompleted(3, receiveTask);
+            Assert.Equal(new byte[] { 1, 2, 3 }, receiveBuffer);
+
+            // another receive
+            byte[] receiveBuffer2 = new byte[3];
+            Task<int> receiveTask2 = AssertTaskPending(channel.ReceiveAsync(receiveBuffer2));
+
+            channel.Send(new byte[] { 4, 5, 6 });
+
+            AssertTaskCompleted(3, receiveTask2);
+            Assert.Equal(new byte[] { 4, 5, 6 }, receiveBuffer2);
+        }
+
+        [Fact]
+        public void Send_then_receive_with_equal_data_size_completes_sync()
+        {
+            MemoryChannel channel = new MemoryChannel();
+
+            channel.Send(new byte[] { 1, 2, 3 });
+
+            byte[] receiveBuffer = new byte[3];
+            AssertTaskCompleted(3, channel.ReceiveAsync(receiveBuffer));
+            Assert.Equal(new byte[] { 1, 2, 3 }, receiveBuffer);
+        }
+
+        [Fact]
+        public void Send_then_receive_with_lower_data_size_completes_sync()
+        {
+            MemoryChannel channel = new MemoryChannel();
+
+            channel.Send(new byte[] { 1, 2, 3 });
+
+            byte[] receiveBuffer = new byte[2];
+            AssertTaskCompleted(2, channel.ReceiveAsync(receiveBuffer));
+            Assert.Equal(new byte[] { 1, 2 }, receiveBuffer);
+        }
+
+        [Fact]
+        public void Send_then_receive_with_greater_data_size_completes_sync()
+        {
+            MemoryChannel channel = new MemoryChannel();
+
+            channel.Send(new byte[] { 1, 2, 3 });
+
+            byte[] receiveBuffer = new byte[4];
+            AssertTaskCompleted(3, channel.ReceiveAsync(receiveBuffer));
+            Assert.Equal(new byte[] { 1, 2, 3, 0 }, receiveBuffer);
+        }
+
+        [Fact]
+        public void Two_sends_then_receive_with_equal_data_size_completes_sync()
+        {
+            MemoryChannel channel = new MemoryChannel();
+
+            channel.Send(new byte[] { 1, 2, 3 });
+            channel.Send(new byte[] { 4, 5, 6 });
+
+            byte[] receiveBuffer = new byte[6];
+            AssertTaskCompleted(6, channel.ReceiveAsync(receiveBuffer));
+            Assert.Equal(new byte[] { 1, 2, 3, 4, 5, 6 }, receiveBuffer);
+        }
+
+        [Fact]
+        public void Two_sends_then_receive_with_greater_data_size_completes_sync()
+        {
+            MemoryChannel channel = new MemoryChannel();
+
+            channel.Send(new byte[] { 1, 2, 3 });
+            channel.Send(new byte[] { 4, 5, 6 });
+
+            byte[] receiveBuffer = new byte[7];
+            AssertTaskCompleted(6, channel.ReceiveAsync(receiveBuffer));
+            Assert.Equal(new byte[] { 1, 2, 3, 4, 5, 6, 0 }, receiveBuffer);
+        }
+
+        [Fact]
+        public void Two_sends_then_three_receives_with_equal_data_size_completes_sync()
+        {
+            MemoryChannel channel = new MemoryChannel();
+
+            channel.Send(new byte[] { 1, 2, 3 });
+            channel.Send(new byte[] { 4, 5, 6 });
+
+            byte[] receiveBuffer = new byte[2];
+            AssertTaskCompleted(2, channel.ReceiveAsync(receiveBuffer));
+            Assert.Equal(new byte[] { 1, 2 }, receiveBuffer);
+
+            byte[] receiveBuffer2 = new byte[2];
+            AssertTaskCompleted(2, channel.ReceiveAsync(receiveBuffer2));
+            Assert.Equal(new byte[] { 3, 4 }, receiveBuffer2);
+
+            byte[] receiveBuffer3 = new byte[2];
+            AssertTaskCompleted(2, channel.ReceiveAsync(receiveBuffer3));
+            Assert.Equal(new byte[] { 5, 6 }, receiveBuffer3);
+        }
+
+        private static Task<TResult> AssertTaskPending<TResult>(Task<TResult> task)
+        {
+            Assert.False(task.IsCompleted, "Task should not be completed.");
+            Assert.False(task.IsFaulted, "Task should not be faulted: " + task.Exception);
+            return task;
+        }
+
+        private static void AssertTaskCompleted<TResult>(TResult expected, Task<TResult> task)
+        {
             // IsCompleted means that the Task has finished executing, either successfully, exceptionally, or due to cancellation.  
             // In other words, IsCompleted is equivalent to the Task.Status being equal to RanToCompletion, Faulted, or Canceled
-            Assert.Equal(TaskStatus.RanToCompletion, receiveTask.Status);
-            Assert.Equal(sendBuffer.Length, receiveBuffer.Length);
-            Assert.Equal(sendBuffer, receiveBuffer);                
+            Assert.Equal(TaskStatus.RanToCompletion, task.Status);
+            Assert.Equal(expected, task.Result);
         }
     }
 }
