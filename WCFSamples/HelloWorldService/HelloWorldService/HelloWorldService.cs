fdbd943755e45d38d4055a8eb735a5ebe2003121
RandolphYao <randolph.yao@gmail.com>
2014-02-13 12:31:43 -0800

    WCF Host Service

---
+++ WCFSamples/HelloWorldService/HelloWorldService/HelloWorldService.cs
@@ -0,0 +1,10 @@
+﻿namespace HelloWorldService
+{
+    public class HelloWorldService : IHelloWorld
+    {
+        public string SayHello(Name person)
+        {
+            return string.Format("Hello {0} {1}", person.FirstName, person.LastName);
+        }
+    }
+}
---
+++ WCFSamples/HelloWorldService/HelloWorldService/IHelloWorld.cs
@@ -0,0 +1,24 @@
+﻿namespace HelloWorldService
+{
+    using System.Runtime.Serialization;
+    using System.ServiceModel;
+
+    // Indicates that an interface or a class defines a service contract in a Windows Communication Foundation (WCF) application.
+    [ServiceContract]
+    public interface IHelloWorld
+    {
+        // operation contract indicates that a method defines an operation that is part of a service contract in a Windows Communication Foundation (WCF) application.
+        [OperationContract]
+        string SayHello(Name person);
+    }
+
+    [DataContract]
+    public class Name
+    {
+        [DataMember]
+        public string FirstName { get; set; }
+
+        [DataMember]
+        public string LastName { get; set; }
+    }
+}
