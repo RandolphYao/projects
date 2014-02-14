2d909dc8808ae9dd1d1771a251f964a0e831e017
RandolphYao <randolph.yao@gmail.com>
2014-02-13 21:09:33 -0800

    Basic Eval Service

---
+++ WCFSamples/EvalService/EvalService/EvalService.cs
@@ -0,0 +1,27 @@
+﻿namespace EvalService
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ServiceModel;
+
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class EvalService : IEvalService
+    {
+        private readonly List<Eval> evals;
+
+        public EvalService()
+        {
+            this.evals = new List<Eval>();
+        }
+
+        public void SubmitEval(Eval eval)
+        {
+            this.evals.Add(eval);
+        }
+
+        public List<Eval> GetEvals()
+        {
+            return this.evals;
+        }
+    }
+}
---
+++ WCFSamples/EvalService/EvalService/IEvalService.cs
@@ -0,0 +1,30 @@
+﻿namespace EvalService
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Runtime.Serialization;
+    using System.ServiceModel;
+
+   [ServiceContract]
+    public interface IEvalService
+    {
+        [OperationContract]
+        void SubmitEval(Eval eval);
+
+        [OperationContract]
+        List<Eval> GetEvals();
+    }
+
+    [DataContract]
+    public class Eval
+    {
+        [DataMember]
+        public string Submit { get; set; }
+
+        [DataMember]
+        public DateTime Timesent { get; set; }
+
+        [DataMember]
+        public string Commments { get; set; }
+    }
+}
