085cc678d95ba0cf462062b8b2ed558261a50301
RandolphYao <randolph.yao@gmail.com>
2014-02-13 16:40:57 -0800

    WCF Client

---
+++ WCFSamples/HelloWorldService/Client/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Client
+{
+    using System;
+
+    using Client.HelloWorldServiceReference;
+
+    using HelloWorldService;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Enter First Name:");
+            string firstname = Console.ReadLine();
+
+            Console.WriteLine("Enter Last Name:");
+            string lastName = Console.ReadLine();
+
+            HelloWorldClient client = new HelloWorldClient("NetTcpBinding_IHelloWorld");
+            Name myName = new Name() { FirstName = firstname, LastName = lastName };
+            Console.WriteLine("Sending Service Request ...");
+            Console.WriteLine(client.SayHelloAsync(myName).Result);     
+            client.Close();
+            Console.ReadLine();
+        }
+    }
+}
